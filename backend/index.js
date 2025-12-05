885b47c0aaebba9ee670e1093420452f10fde4d7
Pintu <pintu@Pintus-Laptop.local>
2025-12-05 22:21:56 +0530

    correct path

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const path = require('path');
-const outfitsRoute = require('./routes/outfits');
+const outfitsRoute = require('./routes/Outfits');
 require('dotenv').config();
 
 
--- client/src/components/ARTryOn.js
+++ client/src/components/ARTryOn.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import OutfitCarousel from "./OutfitCarousel";
 import { Camera } from "@mediapipe/camera_utils";
-import { createPoseModel } from "../utils/poseModel";
+import { createPoseModel } from "../utils/posemodel";
 import * as THREE from "three";
 
 
